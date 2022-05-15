27f07a109fde7175643fee4a44fd242f16f02a3e
Petre Leonard Macamete <petreleonardos@gmail.com>
2022-05-15 06:25:15 +0300

    ok

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import naive from 'naive-ui'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(naive).use(store).use(router).mount('#app')
